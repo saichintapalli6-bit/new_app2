635fd4fdc031300236b86e3ab1b6a47b6545e501
Sai <saichintapalli6@gmail.com>
2026-03-01 17:37:17 +0530

    fix: Define STATIC_ROOT and configure WhiteNoise for Render deployment

--- Smart_Vehicle_Procurement_System_using_Blockchain_Technology/Smart_Vehicle_Procurement_System_using_Blockchain_Technology/settings.py
+++ Smart_Vehicle_Procurement_System_using_Blockchain_Technology/Smart_Vehicle_Procurement_System_using_Blockchain_Technology/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,8 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Required for serving static files on Render
+# Ensure whitenoise is in installed middleware
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
